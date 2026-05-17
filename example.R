262b9a240468d4f0bd3caf755427ef82001b139a
Laura Phillips <laura.m.phillips2012@gmail.com>
2026-05-17 18:18:07 -0400

    1st change for 1st_testingbranch, green

--- example.R
+++ example.R
@@ -3,3 +3,5 @@
 #This is now my second line of changes#
 
 #Ok that actually was my first line^.....still new at this, now this is my second change#
+
+#This is for my 1st_testingbranch, I will  talk about my favorite colors, my main favorite is green#
